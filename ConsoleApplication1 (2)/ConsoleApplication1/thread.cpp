5acc927e59e23e087167bd5ef101164f347a9cac
jjangsean <4066898@naver.com>
2016-11-21 23:13:58 +0900

    folder

---
+++ ConsoleApplication1 (2)/ConsoleApplication1/thread.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "keyevent.h"
+#include <process.h>
+
+using namespace std;
+unsigned int __stdcall mythread(void*)
+{
+	int i = 0;
+	while (1) {
+		cout << GetCurrentThreadId() << ": " << i++ << endl;
+		Sleep(500);
+	}
+	return 0;
+}
+
+unsigned int __stdcall keyEvent(void*) {
+	KeyEvent k;
+	int i;
+	while (1) {
+		i = k.getKey();
+		switch (i) {
+		case 37: cout << "Left key is pressed" << endl; break;
+		case 38:cout << "Up key is pressed" << endl; break;
+		case 39:cout << "Right key is pressed" << endl; break;
+		case 40:cout << "Down key is pressed" << endl; break;
+		}
+	}
+	return 0;
+}
+
+/*int main()
+{
+	HANDLE handleA, handleB;
+
+	handleA = (HANDLE)_beginthreadex(0, 0, &mythread, (void*)0, 0, 0);
+	handleB = (HANDLE)_beginthreadex(0, 0, &keyEvent, (void*)0, 0, 0);
+
+	WaitForSingleObject(handleA, INFINITE);
+	WaitForSingleObject(handleB, INFINITE);
+
+	CloseHandle(handleA);
+	CloseHandle(handleB);
+
+	return 0;
+
+}
+*/
